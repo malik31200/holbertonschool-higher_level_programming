d635a4f7d8b0e88a1d94903cd1cfd640d078331f
malik31200 <yka-vtc@outlook.fr>
2025-09-03 12:52:22 +0200

    Fix condition code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,15 @@ if __name__ == "__main__":
     count = len(argv) - 1
     if count == 0:
         print("0 arguments.")
-    i = 1
-    for value in argv[1:]:
-        print("{}: {}".format(i, value))
-        i += 1
+    elif count == 1:
+        print("1 argument:")
+        i = 1
+        for value in argv[1:]:
+            print("{}: {}".format(i, value))
+            i += 1
+    else:
+        print("{} arguments:".format(count))
+        i = 1
+        for value in argv[1:]:
+            print("{}: {}".format(i, value))
+            i += 1
