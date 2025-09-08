45021b4e0d47bcad061237f430054de15fd9e610
malik31200 <yka-vtc@outlook.fr>
2025-09-08 13:31:35 +0200

    fix space at the end

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in row:
-            print("{:d}".format(i), end=" ")
+        for index, value in enumerate(row):
+            if index != len(row)-1:
+                print("{:d}".format(value), end=" ")
+            else:
+                print("{:d}".format(value), end="")
         print()
