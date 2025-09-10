42175fa8b33aa93710b957c433a12249e8b7a1da
malik31200 <yka-vtc@outlook.fr>
2025-09-10 12:21:10 +0200

    convert roman to arab

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
+
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev_value = 0
+    result = 0
+    for char in roman_string:
+        value = dict[char]
+        if prev_value < value:
+            result -= prev_value
+        else:
+            result += prev_value
+        prev_value = value
+    result += prev_value
+    return result
