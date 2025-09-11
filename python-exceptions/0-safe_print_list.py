dcadcdbb2455947fe004fe6700ff680e23a8b426
malik31200 <yka-vtc@outlook.fr>
2025-09-11 10:18:30 +0200

    fix code move count

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,10 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-
+            count += 1
         except IndexError:
             break
-        else:
-            count += 1
-    print("")
+    print()
     return count
