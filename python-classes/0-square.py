3740b6471d99daa10bc99e5412f7c5b99c384637
malik31200 <yka-vtc@outlook.fr>
2025-09-15 11:08:20 +0200

    an empty squar

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+An empty class that definites a square.
+"""
+
+
+class Square:
+    """Class that represent a empty square(without attributes"""
+    pass
