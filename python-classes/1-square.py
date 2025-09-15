c868b3b576c9379aea3cf963004b8c13792f4eeb
malik31200 <yka-vtc@outlook.fr>
2025-09-15 11:55:13 +0200

    attribute size of a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+a class that definites a square
+"""
+
+
+class Square:
+    """Square with a private instance attribute : size"""
+    def __init__(self, __size):
+        self.__size = __size
