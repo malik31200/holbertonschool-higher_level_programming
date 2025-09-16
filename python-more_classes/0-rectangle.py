8b77650dda2683926024ac1914911e6fd9d4870c
malik31200 <yka-vtc@outlook.fr>
2025-09-16 10:25:03 +0200

    Add Class that definites an empty rectangle + README.md

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+An empty class that definites a rectangle.
+"""
+
+
+class Rectangle:
+    """Class that represent a empty rectangle(without attributes)"""
+    pass
