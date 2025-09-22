40fe0b11910c1d6b7cbff42a75a2c097e06d3310
malik31200 <yka-vtc@outlook.fr>
2025-09-22 11:21:54 +0200

    fix docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""
+0-lookup
+Module that defines the function lookup.
+"""
+
+
 def lookup(obj):
     """
-    Returns he list of available attributes and methods of an object:
+    Returns the list of available attributes and methods of an object:
     Args:
         obj: the object
 
-    Return:
-        list: A list  of attributes an methods name
+    Returns:
+        list: A list  of attributes an methods names
     """
     return dir(obj)
