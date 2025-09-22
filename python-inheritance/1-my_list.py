6580cba15e4a6c603967b9eee5b57340ce5d5293
malik31200 <yka-vtc@outlook.fr>
2025-09-22 11:32:05 +0200

    fix docstring

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -11,7 +11,9 @@ class MyList(list):
     """
     def print_sorted(self):
         """
-        Prints the sorted list
+        Prints the list, but sorted in ascending order.
+
+        This method does not modify the original list.
         """
         list_sorted = sorted(self)
         print(list_sorted)
