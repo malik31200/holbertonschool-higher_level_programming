875c616ff01bd80aed00082e3e5296f0a1da2836
malik31200 <yka-vtc@outlook.fr>
2025-09-22 14:27:50 +0200

    fix docstring

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+5-base_geometry
+Module that defines the class BaseGeometry.
+"""
+
+
 class BaseGeometry:
+    """Class that represent a empty BaseGeometry(without attributes)"""
     pass
