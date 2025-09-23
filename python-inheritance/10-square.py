590c864e3459014893f04d11da44f92c746c2769
malik31200 <yka-vtc@outlook.fr>
2025-09-23 09:33:58 +0200

    add def area

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -72,3 +72,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
