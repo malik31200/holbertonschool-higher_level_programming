5f642ce08e6a35790cad8f05b767da3f3a8ebf2f
malik31200 <yka-vtc@outlook.fr>
2025-09-30 12:35:25 +0200

    Create a class and return a dictionnary

---
+++ python-input_output/9-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module 9-student
+class Student that defines a student
+"""
+
+
+class Student:
+    """
+    Class that defines a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of the Student instance
+        with all its attributes, suitable for JSON serialization.
+        """
+        return self.__dict__.copy()
