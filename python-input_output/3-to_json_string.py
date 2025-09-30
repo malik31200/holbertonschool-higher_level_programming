6210587f903612400756178dff288c9899d3f955
malik31200 <yka-vtc@outlook.fr>
2025-09-30 09:48:01 +0200

    Fix docstring

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module 3-to_json_string
-Returns the JSON representation of an object (string)
+Provides a function that returns the JSON representation of an object.
 """
 
 
@@ -9,4 +9,15 @@ import json
 
 
 def to_json_string(my_obj):
+    """
+    Convert an object to its JSON string representation.
+
+    Args:
+            my_obj (any): The object to serialize. It can be a dict, list,
+                        string, int, bool, or other JSON-serializable type.
+
+    Returns:
+            str: The JSON string representation of the object.
+    """
+
     return json.dumps(my_obj)
