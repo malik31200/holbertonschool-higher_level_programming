2fdeaf49d1d9ab95a220a9e2312a9da2fcd97c96
malik31200 <yka-vtc@outlook.fr>
2025-10-01 10:39:51 +0200

    Feat: serialize and deserialize custom Python objects using the pickle module

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import pickle
+
+
+class CustomObject:
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except (OSError, pickle.PickleError) as e:
+            print(f"Error saving object: {e}")
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+            return obj
+        except (OSError, pickle.PickleError) as e:
+            print(f"Error loading object: {e}")
+            return None
