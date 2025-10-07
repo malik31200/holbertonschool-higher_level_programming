1909384d26606dcca5435b0d12b368bb2cbf0c6a
malik31200 <yka-vtc@outlook.fr>
2025-10-07 09:58:23 +0200

    Convert structured data into other formats, e.g., CSV

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print("Status Code:", response.status_code)
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post["title"])
+    else:
+        print("Failed to fetch posts")
+
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        posts_data = [
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
+            for post in posts
+        ]
+
+        with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(posts_data)
+
+        print("Posts saved to posts.csv")
+    else:
+        print("Failed to fetch posts")
