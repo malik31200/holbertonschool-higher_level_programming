bdd896baa8a2bf7e440cd2886962d4974f1fbb63
malik31200 <yka-vtc@outlook.fr>
2025-10-22 11:00:03 +0200

    cript that lists all records with a score >= 10

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- script that lists all records with a score >= 10
+--  display both the score and the name
+-- ordered by score (top first)
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC
