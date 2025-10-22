21ba54250652b629f3857f458823152ee5717d22
malik31200 <yka-vtc@outlook.fr>
2025-10-22 16:11:23 +0200

    Create user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,11 @@
+-- create user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all previleges on all data bases
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost'
+WITH GRANT OPTION;
+
+-- apply changes immediatly
+FLUSH PRIVILEGES;
