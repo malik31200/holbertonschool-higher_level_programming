4a96217ece71b03fec12cc9890e50650a64b8c6f
malik31200 <yka-vtc@outlook.fr>
2025-10-22 16:13:54 +0200

    Fix syntaxe

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,9 @@
 -- create user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all previleges on all data bases
+-- Grant all privileges on all data bases
 GRANT ALL PRIVILEGES ON *.*
-TO 'user_0d_1'@'localhost'
-WITH GRANT OPTION;
+TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- apply changes immediatly
 FLUSH PRIVILEGES;
