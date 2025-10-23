08657050361ce884aa49b074561614d430fcdd48
malik31200 <yka-vtc@outlook.fr>
2025-10-23 14:49:12 +0200

    Fix style

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- List all shows with at least one genre
-SELECT title, genre_id
-FROM tv_shows
+SELECT title, genre_id FROM tv_shows
 INNER JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY title ASC, genre_id ASC;
