78594070ef9152ae9265af95c90ed98a96951158
malik31200 <yka-vtc@outlook.fr>
2025-10-23 14:56:41 +0200

    fix style

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
--- List all shows with at least one genre
+-- Lists all shows in hbtn_0d_tvshows with at least one genre linked
 SELECT title, genre_id FROM tv_shows
-INNER JOIN tv_show_genres 
-ON tv_shows.id = tv_show_genres.tv_show_id
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title ASC, genre_id ASC;
