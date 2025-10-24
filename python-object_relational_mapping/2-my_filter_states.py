49542f9b9ba5d036ebdd0b18e75fb6c74d7d4ff3
malik31200 <yka-vtc@outlook.fr>
2025-10-24 12:50:49 +0200

    list states matching argument from hbtn_0e_0_usa

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # retrieve the arguments
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name_searched = argv[4]
+
+    # connect to Mysl database
+    connexion = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+
+    # Create a cursor
+    cur = connexion.cursor()
+
+    # Executing SQL query with format
+    cur.execute(
+                "SELECT * FROM states "
+                "WHERE states.name = '{}' "
+                "ORDER BY states.id ASC".format(state_name_searched)
+                )
+
+    # Displaying results as tuples (id, name)
+    for row in cur.fetchall():
+        print(row)
+
+    # Closing the cursor and connection
+    cur.close
+    connexion.close
