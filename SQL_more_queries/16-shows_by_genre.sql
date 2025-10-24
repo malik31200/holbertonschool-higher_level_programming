3fca7e2896bf9e69373ebafe6da7242f5bd2f1a5
malik31200 <yka-vtc@outlook.fr>
2025-10-24 09:55:29 +0200

    Fix add LEFT to JOIN

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- lists all shows, and all genres linked
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres 
+LEFT JOIN tv_genres 
 ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
