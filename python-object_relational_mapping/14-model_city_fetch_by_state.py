58daf810656b33b6bb8382afbe6c365cc55a4a93
malik31200 <yka-vtc@outlook.fr>
2025-10-26 21:03:39 +0100

    add () to {city.id}

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -24,6 +24,6 @@ if __name__ == "__main__":
     cities = session.query(City).order_by(City.id.asc()).all()
 
     for city in cities:
-        print(f"{city.state.name}: {city.id} {city.name}")
+        print(f"{city.state.name}: ({city.id}) {city.name}")
 
     session.close()
