02b8c1c1abe1f2087f5877e86f79d6f7f39837c0
malik31200 <yka-vtc@outlook.fr>
2025-10-26 15:30:17 +0100

    fix style

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,7 +4,7 @@ lists all State objects from the database hbtn_0e_6_usa
 """
 from sys import argv
 from sqlalchemy import create_engine
-from model_state import State
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
         print(f"{state.id}: {state.name}")
 
     session.close()
