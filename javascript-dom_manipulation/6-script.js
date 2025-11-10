f019514af43d673b043562a7620543e98d2fbcb6
malik31200 <yka-vtc@outlook.fr>
2025-11-10 10:42:39 +0100

    fetches the character name from an URL

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,8 @@
+const character = document.querySelector('#character');
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then((response) => {
+    return response.json();
+  }).then((data) => {
+    character.textContent = data.name;
+  });
